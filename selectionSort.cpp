8710e62a10482827be1a173b4abd0deef12e1ce2
kashishsharma1 <60250550+kashishsharma1@users.noreply.github.com>
2021-04-02 22:53:44 +0530

    Add files via upload

---
+++ selectionSort.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+ 
+void swap(int arr[], int i, int j ){
+    
+    int temp = arr[i];
+     arr[i] = arr[j];
+     arr[j] = temp;
+
+}
+
+int main(){
+    int n;
+    cin >> n;
+
+    int arr[n];
+    for(int i = 0; i < n; i++){
+        cin>>arr[i];
+    }
+    
+    for(int i = 0; i < n-1; i++){
+        for(int j = i + 1; j < n; j++){
+            if(arr[i] > arr[j])
+                swap(arr,i,j);
+        }
+    }
+    for(int i = 0; i < n; i++){
+        cout<<arr[i]<<" ";
+    }cout<<endl;
+   return 0; 
+}
